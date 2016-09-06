0f25196cbd70a466838b9c320ec0ab4e8b7e4170
prodocik <prodocik@gmail.com>
2016-09-06 20:40:07 +0300

    UI autolayout WIP

--- quiz_3/ViewController.swift
+++ quiz_3/ViewController.swift
@@ -9,19 +9,13 @@ class ViewController: UIViewController {
     var quote = -1
     var highScore = 0
     var question = [[[String]]]()
-    //let question = [[["01. Как поймать суслика"],["Руками", "Клеткой","Суслик сам поймаетс","Это не реально" ],["0","1","4", "6"]],[["02. Где у рукожопа голова?"],["Там где нет мозгов", "В раковине","Там где должны быть руки","На месте" ],["4","2","1", "0"]],[["03. Что, Где и Когда?"],["Ниче, нигде и никогда", "Что-то где-то когда-то","Не в курсах","Дратути" ],["3","1","6", "0"]],[["04. Куда дели Билли Боба"],["Он ушел", "Где-то по середине лета","Прости прощай!","Штэ?" ],["2","2","1", "4"]]]
-    
-
-    
-    
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let path = NSBundle.mainBundle().pathForResource("TableData", ofType: "plist")
         let dict = NSDictionary(contentsOfFile: path!)
-        
         question = dict!.objectForKey("mainArray") as! [[[String]]]
-        print (question)
         generateQuestion(showrandom())
     }
     
@@ -37,31 +31,24 @@ class ViewController: UIViewController {
     
     
     func showrandom() -> Int{
-        
         quote += 1
         if quote >= question.count {
             quote = 3
-    
             self.performSegueWithIdentifier("segue", sender: nil)
             quote = 0
             highScore = 0
         }
-        
         return quote
-        
     }
     
   
     
     func generateQuestion (questionNumber: Int) {
-        
-
         QuestionLabel.text = question[questionNumber][0][0]
         Button1.setTitle(question[questionNumber ][1][0], forState: .Normal)
         Button2.setTitle(question[questionNumber ][1][1], forState: .Normal)
         Button3.setTitle(question[questionNumber ][1][2], forState: .Normal)
         Button4.setTitle(question[questionNumber ][1][3], forState: .Normal)
-
     }
     
     
@@ -74,7 +61,6 @@ class ViewController: UIViewController {
     @IBAction func Button2Action(sender: UIButton) {generateQuestion(showrandom());generateHighScore(2)}
     @IBAction func Button3Action(sender: UIButton) {generateQuestion(showrandom());generateHighScore(3)}
     @IBAction func Button4Action(sender: UIButton) {generateQuestion(showrandom());generateHighScore(4)}
-    
 }
 
 
